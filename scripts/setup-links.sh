a11ca261badb841e8887f0e4f3ba69c1991f861f
Joel Elizaga <jelizaga@discovery-1.hsd1.wa.comcast.net>
2022-06-10 17:41:11 -0700

    Added espanso base config, espanso.

--- scripts/setup-links.sh
+++ scripts/setup-links.sh
@@ -4,6 +4,8 @@
 # Creates symbolic links between various program's config locations and their
 # actual configs located in ~/.dotfiles/dots.
 # SYNTAX: ln -s [destination] [point of entry]
+# bashrc #######################################################################
+ln -s ~/.dotfiles/dots/bashrc ~/.bashrc
 # bash_aliases #################################################################
 ln -s ~/.dotfiles/dots/bash_aliases ~/.bash_aliases
 # mpd ##########################################################################
@@ -15,4 +17,4 @@ ln -s ~/.dotfiles/dots/ncmpcpp.conf ~/.ncmpcpp/config
 # code #########################################################################
 ln -s ~/.dotfiles/dots/code.conf ~/.config/Code/User/settings.json
 # espanso ######################################################################
-ln -s ~/.dotfiles/dots/espanso-base.conf ~/.config/espanso/match/base.yml
+ln -s ~/.dotfiles/dots/espanso-base.yml ~/.config/espanso/match/base.yml
