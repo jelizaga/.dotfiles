4408c080ccac4bcb59f8682e7980cd28110c3124
Joel Elizaga <joel@elizaga.org>
2022-09-29 12:37:05 -0700

    Removed simpler scripts & replaced with initial interactive ones.

--- scripts/setup-ncmpcpp.sh
+++
@@ -1,5 +0,0 @@
-# Disables the mpd system service.
-# Now mpd will use `~/.mpd/mpd.conf` instead of the system-wide configuration 
-# in `/etc/mpd.conf`.
-sudo systemctl disable mpd.service
-sudo systemctl disable mpd.socket
